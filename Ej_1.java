fbc82692d4636fdf076f364b3a81a08afda4f3ab
Fernando-Hinostroza <fernando.hinostroza@estudiante.ucsm.edu.pe>
2025-08-19 16:35:33 -0500

    Create Ej_1.java

---
+++ Ej_1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Propuesto_1 {
+    static int sumaArreglo(int[] arr) {
+        int suma = 0;
+        for (int num : arr) {
+            suma += num;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+
+        // Validamos que el tamaño del arreglo sea positivo
+        while (true) {
+            System.out.print("Ingrese el tamaño del arreglo (>0): ");
+            n = sc.nextInt();
+            if (n > 0) break;
+            System.out.println("❌ Error: el tamaño debe ser mayor a 0.");
+        }
+
+        int[] arr = new int[n];
+        // Llenamos el arreglo validando que se ingresen números enteros
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingrese el valor en la posición " + (i+1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("La suma del arreglo es: " + sumaArreglo(arr));
+    }
+}
